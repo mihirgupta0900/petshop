2caf432f03f5da5b1b29327d671cb06e226c231f
Mihir Gupta <mihirgupta0900@gmail.com>
2021-02-24 13:21:07 +0530

    deploying to heroku

--- ormconfig.js
+++ ormconfig.js
@@ -1,8 +1,8 @@
 require("dotenv").config()
-const { DATABASE_URI } = process.env
+const { DATABASE_URL } = process.env
 const parse = require("pg-connection-string").parse
 
-const config = parse(DATABASE_URI)
+const config = parse(DATABASE_URL)
 const { host, password, database, port, user } = config
 
 const isTsNode = !!process.argv.find((val) => val.includes("ts-node"))
@@ -16,7 +16,7 @@ module.exports = {
   database: database,
   synchronize: false,
   entities: [isTsNode ? "src/entity/*.ts" : "dist/entity/*.js"],
-  migrations: [isTsNode ? "src/migrations/*.ts" : "compiled/migrations/*.js"],
+  migrations: [isTsNode ? "src/migrations/*.ts" : "dist/migrations/*.js"],
   cli: {
     migrationsDir: "./src/migrations",
   },
